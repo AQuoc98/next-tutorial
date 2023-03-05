4304fc26dd80d05b194612589ad94403517581e4
Anh Quốc <ken@192.168.102.2>
2023-03-05 22:20:12 +0700

    event-app/feat/data: Working on "all events" page

--- event-app/pages/events/index.js
+++ event-app/pages/events/index.js
@@ -1,11 +1,11 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-import { getAllEvents } from "@/data/dummy-data";
+import { getAllEvents } from "@/helpers/api-util";
 import { useRouter } from "next/router";
 
-export default function AllEventsPage() {
+export default function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
@@ -18,4 +18,15 @@ export default function AllEventsPage() {
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
