254b3c7d9e59908c4ddcf6ae78f1f6e775c574b3
Anh Quốc <ken@192.168.102.2>
2023-03-10 16:34:53 +0700

    blog/feat/featured-posts: adding posts grid, featured posts

--- blog-app/pages/index.js
+++ blog-app/pages/index.js
@@ -1,10 +1,22 @@
+import FeaturedPosts from "@/components/home-page/featured-posts";
 import Hero from "@/components/home-page/hero";
 import { Fragment } from "react";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-stared-with-nextjs",
+    title: "Getting startd with NextJS",
+    image: "getting-stared-nextjs.png",
+    excerpt: "NextJS is a React framework",
+    date: "2023-03-03",
+  },
+];
 
 export default function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
