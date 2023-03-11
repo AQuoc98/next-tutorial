b8a11eac354f64d9b040c3ad60318b3d91c0f96d
Anh Quốc <ken@192.168.102.2>
2023-03-11 21:50:10 +0700

    blog/feat/all-posts-page: adding all posts

--- blog-app/pages/posts/index.js
+++ blog-app/pages/posts/index.js
@@ -1,1 +1,15 @@
-export default function AllPostsPage() {}
+import AllPosts from "@/components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-stared-with-nextjs",
+    title: "Getting startd with NextJS",
+    image: "getting-stared-nextjs.png",
+    excerpt: "NextJS is a React framework",
+    date: "2023-03-03",
+  },
+];
+
+export default function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
